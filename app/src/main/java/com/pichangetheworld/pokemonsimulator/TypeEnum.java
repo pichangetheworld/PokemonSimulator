b996384831522f2cbc3ffecf4023e320b8554ffc
pchan <pichangetheworld@gmail.com>
2015-02-04 20:07:00 +0900

    Type enum.

---
+++ app/src/main/java/com/pichangetheworld/pokemonsimulator/TypeEnum.java
@@ -0,0 +1,26 @@
+package com.pichangetheworld.pokemonsimulator;
+
+/**
+ * Eminent Domain AS
+ * Author: pchan
+ * Date: 04/02/2015
+ */
+public enum TypeEnum {
+    NORMAL,
+    FIGHTING,
+    FLYING,
+    POISON,
+    GROUND,
+    ROCK,
+    BUG,
+    GHOST,
+    STEEL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    PSYCHIC,
+    ICE,
+    DRAGON,
+    DARK
+}
