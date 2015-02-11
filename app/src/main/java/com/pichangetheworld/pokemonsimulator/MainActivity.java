ca1394c735c81a6b80839475ad3f9265b4ae6059
pchan <pichangetheworld@gmail.com>
2015-02-11 18:45:39 +0900

    Drawables too big.

--- app/src/main/java/com/pichangetheworld/pokemonsimulator/MainActivity.java
+++ app/src/main/java/com/pichangetheworld/pokemonsimulator/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
     final AdapterView.OnItemSelectedListener onSelectionChanged =
@@ -21,17 +24,37 @@ public class MainActivity extends ActionBarActivity {
                             defType2Selector.getSelectedItemPosition()
                     );
                     Log.d("MainActivity", "New multiplier is " + result);
+                    updateResult(getResultIndex(result));
                 }
 
                 @Override
                 public void onNothingSelected(AdapterView<?> parent) {}
             };
 
+    final int[] ARROW_IDS = {
+            R.drawable.damage0,
+            R.drawable.damage025,
+            R.drawable.damage05,
+            R.drawable.damage1,
+            R.drawable.damage2,
+            R.drawable.damage4
+    };
+
+    final int[] MULTIPLIER_IDS = {
+            R.drawable.x0,
+            R.drawable.x025,
+            R.drawable.x05,
+            R.drawable.x1,
+            R.drawable.x2,
+            R.drawable.x4
+    };
+
     Spinner atkTypeSelector;
     Spinner defType1Selector;
     Spinner defType2Selector;
 
     ImageView arrow;
+    ImageView multiplier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +64,29 @@ public class MainActivity extends ActionBarActivity {
         atkTypeSelector = (Spinner) findViewById(R.id.spinner3);
         defType1Selector = (Spinner) findViewById(R.id.spinner);
         defType2Selector = (Spinner) findViewById(R.id.spinner2);
-        arrow = (ImageView) findViewById(R.id.arrow);
+//        arrow = (ImageView) findViewById(R.id.arrow);
+//        multiplier = (ImageView) findViewById(R.id.multiplier);
 
         atkTypeSelector.setOnItemSelectedListener(onSelectionChanged);
         defType1Selector.setOnItemSelectedListener(onSelectionChanged);
         defType2Selector.setOnItemSelectedListener(onSelectionChanged);
     }
 
+    final List<Double> indices = Arrays.asList(
+            0.0,
+            0.25,
+            0.5,
+            1.0,
+            2.0,
+            4.0
+    );
 
+    private int getResultIndex(double result) {
+        return indices.indexOf(result);
+    }
+
+    private void updateResult(int i) {
+//        arrow.setImageDrawable(getResources().getDrawable(ARROW_IDS[i]));
+//        multiplier.setImageDrawable(getResources().getDrawable(MULTIPLIER_IDS[i]));
+    }
 }
